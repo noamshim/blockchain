fdef05f6abf81f6e887b07fee0e6a29a14d86a0d
noam <nooamce@gmail.com>
2020-12-02 18:17:52 +0200

    start

---
+++ customer.js
@@ -0,0 +1,9 @@
+const onSubmit = function () {
+    let id = document.getElementById("customerPackageID").innerHTML;
+    if(id == null || id.length === 0){
+        document.getElementById("customerError").innerHTML = "Invalid package ID";
+    }
+    else{
+
+    }
+}
